cc8466919bad0acabc3f548cabba96e1c1510a52
YasmineMarzouk <yasmine2marzouk@gmail.com>
2023-02-13 15:12:54 +0100

    add config and helpers files

---
+++ config/todoList.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | TodoList Configuration.
+    |--------------------------------------------------------------------------
+    |
+    | This is an example of a custom configuration file for a specific resource.
+    | this file will bring together all the data that will help us to configure
+    | a TodoList page.
+    |
+    */
+
+];
